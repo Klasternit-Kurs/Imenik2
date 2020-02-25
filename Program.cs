644dad38a31f2b05e2a6fa212607cc2d94085fae
Dragan Pepic <d.pepic@hotmail.com>
2020-02-25 17:57:35 +0100

    Izmene

--- Program.cs
+++ Program.cs
@@ -27,8 +27,10 @@ namespace Imenik
 						}
 						break;
 					case '3':
+						Izmena();
 						break;
 					case '4':
+						Brisanje();
 						break;
 					case '5':
 						Environment.Exit(2);
@@ -38,6 +40,48 @@ namespace Imenik
 						break;
 				}
 			} while (true);
+		}
+
+		static void Izmena()
+		{
+			//TODO kao i kod brisanja, napraviti da radi sa indeksima
+			//+ da moze da se menja i broj telefona a ne samo ime i prezime :) 
+
+			Console.Write("\nUnesite broj telefona: ");
+			string br = Console.ReadLine();
+			if (Osoba.ProveriTelefon(br, out Osoba o))
+			{
+				Console.Write("\nUnesite ime i prezime: ");
+				string[] imeIprezime = Console.ReadLine().Split(' ');
+				if (imeIprezime.Length != 2)
+				{
+					Console.WriteLine("Greska u unosu!");
+					return;
+				}
+				o.ime = imeIprezime[0];
+				o.prezime = imeIprezime[1];
+			}
+			else
+			{
+				Console.WriteLine("Ne postoji broj :(");
+			}
+		}
+
+		static void Brisanje()
+		{
+			//TODO napraviti brisanje preko indeksa koristenjem
+			//for petlje i RemoveAt
+
+			Console.Write("\nUnesite broj telefona: ");
+			string br = Console.ReadLine();
+			if (Osoba.ProveriTelefon(br, out Osoba o))
+			{
+				Osoba.Imenik.Remove(o);
+			} else
+			{
+				Console.WriteLine("Ne postoji broj :(");
+			}
+
 		}
 
 		static void Unos()
@@ -56,8 +100,15 @@ namespace Imenik
 			//br.StartsWith("+381"); -- vraca true ako pocinje sa +381
 			Console.Write("Unesite broj: ");
 			string br = Console.ReadLine();
-			
-			new Osoba(imeIprezime[0], imeIprezime[1], br);
+
+			if (Osoba.ProveriTelefon(br, out _))
+			{
+				Console.WriteLine("Telefon vec postoji!");
+			}
+			else
+			{
+				new Osoba(imeIprezime[0], imeIprezime[1], br);
+			}
 		}
 
 		static void StampajMeni()
@@ -78,6 +129,21 @@ namespace Imenik
 
 		public string ime, prezime, broj;
 		//public string prezime;
+
+
+		public static bool ProveriTelefon(string t, out Osoba ob)
+		{
+			foreach (Osoba o in Imenik)
+			{
+				if (o.broj == t)
+				{
+					ob = o;
+					return true;
+				}
+			}
+			ob = null;
+			return false;
+		}
 
 		public Osoba(string i, string p, string b) 
 		{
